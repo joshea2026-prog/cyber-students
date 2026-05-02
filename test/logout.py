eaf35a142eb4d8f41282296cc76a51562c57593a
joshea2026-prog <joshea.ketb2023@gmail.com>
2026-05-02 14:10:51 +0100

    Add files via upload

--- test/logout.py
+++ test/logout.py
@@ -1,12 +1,14 @@
+import json
 from json import dumps
-from tornado.escape import json_decode
 from tornado.httputil import HTTPHeaders
 from tornado.ioloop import IOLoop
 from tornado.web import Application
 
 from api.handlers.logout import LogoutHandler
+from api.handlers.crypto_utils import hash_password, encrypt_field, hash_token
 
 from .base import BaseTest
+
 
 class LogoutHandlerTest(BaseTest):
 
@@ -16,31 +18,49 @@ class LogoutHandlerTest(BaseTest):
         super().setUpClass()
 
     async def register(self):
+        display_name_ct, display_name_iv = encrypt_field('testDisplayName')
+        address_ct,      address_iv      = encrypt_field('123 Test Street, Cork')
+        dob_ct,          dob_iv          = encrypt_field('01-01-1990')
+        phone_ct,        phone_iv        = encrypt_field('+353871234567')
+        disabilities_ct, disabilities_iv = encrypt_field(json.dumps(['dyslexia', 'ADHD']))
+
         await self.get_app().db.users.insert_one({
-            'email': self.email,
-            'password': self.password,
-            'displayName': 'testDisplayName'
+            'email':           self.email,
+            'password':        hash_password(self.plain_password),
+            'displayName':     display_name_ct,
+            'displayName_iv':  display_name_iv,
+            'address':         address_ct,
+            'address_iv':      address_iv,
+            'dateOfBirth':     dob_ct,
+            'dateOfBirth_iv':  dob_iv,
+            'phoneNumber':     phone_ct,
+            'phoneNumber_iv':  phone_iv,
+            'disabilities':    disabilities_ct,
+            'disabilities_iv': disabilities_iv,
         })
 
     async def login(self):
         await self.get_app().db.users.update_one({
             'email': self.email
         }, {
-            '$set': { 'token': self.token, 'expiresIn': 2147483647 }
+            '$set': {
+                'token':     hash_token(self.plain_token),
+                'expiresIn': 2147483647
+            }
         })
 
     def setUp(self):
         super().setUp()
 
-        self.email = 'test@test.com'
-        self.password = 'testPassword'
-        self.token = 'testToken'
+        self.email          = 'test@test.com'
+        self.plain_password = 'testPassword'
+        self.plain_token    = 'testToken'
 
         IOLoop.current().run_sync(self.register)
         IOLoop.current().run_sync(self.login)
 
     def test_logout(self):
-        headers = HTTPHeaders({'X-Token': self.token})
+        headers = HTTPHeaders({'X-Token': self.plain_token})
         body = {}
 
         response = self.fetch('/logout', headers=headers, method='POST', body=dumps(body))
@@ -60,7 +80,7 @@ class LogoutHandlerTest(BaseTest):
         self.assertEqual(400, response.code)
 
     def test_logout_twice(self):
-        headers = HTTPHeaders({'X-Token': self.token})
+        headers = HTTPHeaders({'X-Token': self.plain_token})
         body = {}
 
         response = self.fetch('/logout', headers=headers, method='POST', body=dumps(body))
